a14bae282dc1c720589535f44fdca2db966ece4b
Joshua Weinstein <weinsteinconsultant@gmail.com>
2020-08-03 11:42:38 -0400

    Finished with Playing in DOM

--- index.js
+++ index.js
@@ -7,7 +7,22 @@
 
 //resolve // QUESTION 1 here
 
+const x = 'X';
+const o = 'O';
+a1Sauce = document.getElementById('a-1');
+var button = document.createElement('button');
+button.innerText = 'Click Me';
+a1Sauce.appendChild(button);
+button.className += 'square';
+button.addEventListener('click',xoXO);
 
+function xoXO() {
+   if (button.innerText == x){
+       button.innerText = o; 
+    } else {
+    button.innerText = x;
+   }
+}
 
 
 //------------------------Question 2 ---------------------------
@@ -16,10 +31,31 @@
 //you also need to create a button that makes the remove action of the element selected
 // append the new list as a child of the element with the 'id = a-2'
 // append the button as a child of the element with the 'id = a-2'
-const colors = [ 'red' , 'white', 'black' , 'green' , 'orange'];
+
+    var pullDiv = document.getElementById('a-2'); 
+    const colors = ['Red' , 'White', 'Black', 'green', 'Orange']; 
+    var colorList = document.createElement('select');
+    
+    pullDiv.appendChild(colorList);
+    for (var i = 0; i < colors.length; i++) { 
+        var optn = colors[i]; 
+        var el = document.createElement("option"); 
+        el.textContent = optn; 
+        el.value = optn; 
+        colorList.appendChild(el); 
+    };
+    
+    var buttonC = document.createElement('button');
+    buttonC.innerHTML = 'Delete Button';
+      pullDiv.appendChild(buttonC);
+      buttonC.addEventListener('click', function() {
+      colorList.remove(colorList.selectedIndex);
+});
 
 
+    
 
+    
 
 //resolve // QUESTION 2 here
 
@@ -35,11 +71,24 @@ const colors = [ 'red' , 'white', 'black' , 'green' , 'orange'];
 //hint: the volumen of a sphere is ((4/3) × π × r^3) and the surface area is (4 × π × r^2)
 
 
-const calculate_sphere = () =>{
-
+const calculate_sphere = (event) => { 
+    event.preventDefault()
+    var radius = document.getElementById('radius').value;
+    radius = Math.abs(radius);
+    var volume;
+    volume = (4/3) * Math.PI * Math.pow(radius, 3);
+    volume = volume.toFixed(4);
+    document.getElementById("volume").value = volume;
+    var area;
+    area = 4 * Math.PI * Math.pow(radius, 2);
+    area = area.toFixed(4);
+    document.getElementById("area").value = area;
+    return false;
  }
-
-window.onload = document.getElementById('MyForm').onsubmit = calculate_sphere; // this execute the volume_sphere function each time the calculate (submit) button is clicked
+const myForm = document.getElementById('MyForm');
+myForm.addEventListener('submit', calculate_sphere);
+//calculate_sphere.getElementById(value);
+//window.onload = document.getElementById(submit).onsubmit = calculate_sphere; // this execute the volume_sphere function each time the calculate (submit) button is clicked
 
 
 
@@ -48,4 +97,45 @@ window.onload = document.getElementById('MyForm').onsubmit = calculate_sphere; /
 //Now in this exercise we want you to create 3 buttons, each with a click action that will hide the respective question's answer above. So if you click the "Hide Question 1" button it will hide from the DOM the answer to your first exercise. If you click it again it will show the answer. 
 
 
-//resolve // QUESTION 4 here
+//resolve // QUESTION 4 here 
+questionA = document.getElementById('a-1');
+buttonA = document.getElementById("a-4");            
+buttonOne = document.createElement("BUTTON");
+buttonOne.innerText = 'Hide or Show Answer for Number One';
+buttonA.appendChild(buttonOne);
+buttonOne.addEventListener('click', myFunctionA);
+function myFunctionA() {
+     if (questionA.style.display == "none") {
+       questionA.style.display = "block";
+     } else {
+       questionA.style.display = "none";
+     }}
+  
+     questionB = document.getElementById('a-2');
+     buttonB = document.getElementById("a-4");            
+     buttonTwo = document.createElement("BUTTON");
+     buttonTwo.innerText = 'Hide or Show Answer for Number Two';
+     buttonB.appendChild(buttonTwo);
+     buttonTwo.addEventListener('click', myFunctionB);
+     function myFunctionB() {
+          if (questionB.style.display == "none") {
+            questionB.style.display = "block";
+          } else {
+            questionB.style.display = "none";
+          }}
+         
+          questionD = document.getElementById('a-3');
+          buttonD = document.getElementById("a-4");            
+          buttonThree = document.createElement("BUTTON");
+          buttonThree.innerText = 'Hide or Show Answer for Number Three';
+          buttonD.appendChild(buttonThree);
+          buttonThree.addEventListener('click', myFunctionC);
+          function myFunctionC() {
+               if (questionD.style.display == "none") {
+                 questionD.style.display = "block";
+               } else {
+                 questionD.style.display = "none";
+               }}
+               console.log(questionA)
+          
+
